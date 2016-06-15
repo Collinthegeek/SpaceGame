c24be36ca03e0a4ab9ed5bd7fed6d56fd44f683c
Collin Norwood <Collinthegeek@users.noreply.github.com>
2016-06-14 20:26:19 -0500

    Updated.. health is broke

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pygame, sys, random
 from pygame.locals import *
+pygame.font.init
+pygame.init()
+screen = pygame.display.set_mode((550, 600))
+health=5
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 550)
+font = pygame.font.SysFont("monospace", 55)
+pygame.display.set_caption('Space Game')
+background = pygame.Surface(screen.get_size())
+background.fill((0, 0, 0))
+healthlabel = font.render(str(health), 1, (255,255,255))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -8,13 +19,11 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.image.get_rect(
             center=(275,500))
-
     def update(self, pressed_keys):
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-6, 0)
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(6, 0)
-
         if self.rect.left < 0:
             self.rect.left = 0
         elif self.rect.right > 800:
@@ -23,45 +32,30 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         elif self.rect.bottom >= 600:
             self.rect.bottom = 600
-
-
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
-        self.image = pygame.image.load('img/ship.png').convert()
+        self.image = pygame.image.load('img/enemy.png').convert()
         self.image.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.image.get_rect(
             center=(random.randint(0,550-32),0))
-
     def update(self):
-        self.rect.move_ip(0, 4)
+        self.rect.move_ip(0, 2)
         if self.rect.bottom > 500:
             self.kill()
-            sys.exit()
-
+            health-=1
 class Bullet(pygame.sprite.Sprite):
     def __init__(self):
         super(Bullet, self).__init__()
-        self.image = pygame.image.load('img/ship.png').convert()
+        self.image = pygame.image.load('img/bullet.png').convert()
         self.image.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.image.get_rect(
-            center=(player.rect.x+16,player.rect.y+16))
-
+            center=(player.rect.x+8,player.rect.y+8))
     def update(self):
         self.rect.move_ip(0, -8)
         if self.rect.right < 0:
             self.kill()
-
-pygame.init()
-screen = pygame.display.set_mode((550, 600))
-ADDENEMY = pygame.USEREVENT + 1
-pygame.time.set_timer(ADDENEMY, 550)
-
 player = Player()
-
-background = pygame.Surface(screen.get_size())
-background.fill((0, 0, 0))
-
 enemies = pygame.sprite.Group()
 bullets=pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
@@ -85,16 +79,18 @@ while True:
             all_sprites.add(new_enemy)
 
     screen.blit(background, (0, 0))
+    screen.blit(healthlabel, (500, 10))
     pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
     enemies.update()
     bullets.update()
     for entity in all_sprites:
         screen.blit(entity.image, entity.rect)
-
     if pygame.sprite.spritecollideany(player, enemies):
-        sys.exit()
+        health-=1
     if pygame.sprite.groupcollide(bullets, enemies, True, True, collided = None):
         pass
+    if health==0:
+        sys.exit()
 
     pygame.display.flip()
